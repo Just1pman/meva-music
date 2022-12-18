b0a28284aae5314beb2e1b70619e97dc37884ca4
alexander olhovik <just1pman@gmail.com>
2022-12-18 23:53:58 +0300

    fix

--- wp-app/wp-content/themes/mevamusic/header.php
+++ wp-app/wp-content/themes/mevamusic/header.php
@@ -6,6 +6,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <title><?php wp_title('«', true, 'right'); ?><?php bloginfo('name'); ?></title>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
+    <meta name="robots" content="all">
+    <meta name="keywords" content="Студия звукозаписи Гродно записать трек сведение мастеринг вокал гродно записать трек ">
+    <meta property="og:type" content="business.business">
+    <meta property="og:title" content="Студия звукозаписи Гродно">
+    <meta property="og:url" content="https://mewa.by">
+    <meta property="og:image" content="https://mewa.by/wp-content/uploads/2022/11/d7cf82_b93fb006677c407b9c7780a705dd7b16_mv2.jpg">
+    <meta property="og:site_name" content="Mewa">
+    <meta property="og:locale" content="ru_RU">
+    <meta property="og:type" content="website">
+    <meta property="og:image:secure_url" content="https://mewa.by/wp-content/uploads/2022/11/d7cf82_b93fb006677c407b9c7780a705dd7b16_mv2.jpg">
+    <link rel="preload" href="https://mewa.by/wp-content/uploads/2022/11/d7cf82_b93fb006677c407b9c7780a705dd7b16_mv2.jpg" as="image">
+    <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "Organization",
+            "brand": "Mewa",
+            "name": "Студия звукозаписи Гродно",
+            "alternateName": "Mewa",
+            "description": "Поможем записать трек в Гродно",
+            "logo": "https://mewa.by/wp-content/uploads/2022/11/icons8-music-1.svg",
+            "url": "https://mewa.by/",
+
+            "department": [{
+                "@context": "http://schema.org",
+                "@type": "Organization",
+
+                "location": {
+                    "@type": "Place",
+
+                    "address": {
+                        "@type": "PostalAddress",
+                        "addressLocality": "Гродно",
+                        "streetAddress": "Беларусь, Гродно, улица Победы д. 19б",
+                        "postalCode": "230026"
+                    },
+
+                    "geo": {
+                        "@type": "GeoCoordinates",
+                        "latitude": "53.656697",
+                        "longitude": "23.825477"
+                    }
+                },
+
+                "telephone": [
+                    "+375 (29) 286-22-60"
+                ]
+            }],
+
+            "email": "375292862260@mail.ru"
+        }
+    </script>
+
     <?php wp_head(); ?>
 </head>
 
